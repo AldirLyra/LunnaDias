db420dc6ed67bd43b721378ff9c5735b4750542e
Aldir Lyra <aldir_lyra@hotmail.com>
2023-01-27 00:40:32 -0300

    ajustes de janeiro pt 3, adicionando regex para email

--- js/validateForm.js
+++ js/validateForm.js
@@ -2,9 +2,10 @@ var email = document.getElementById('email');
 var ebook1 = document.getElementById('ebook1');
 var ebook2 = document.getElementById('ebook2');
 var ebook3 = document.getElementById('ebook3');
+var exp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 function updateSubmitBtn() {
-    const emailValue = email.value.trim();
+    const emailValue = email.value.trim().match(exp);
     debugger;
     if (emailValue) {
         ebook1.removeAttribute('disabled');
